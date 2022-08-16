ce971d40ab36f36e3ce395fb2cd05db3c1622e32
snowjelly <broadwellconnor@gmail.com>
2022-08-16 17:29:45 -0500

    basic functions added

---
+++ script.js
@@ -0,0 +1,27 @@
+const add = (num1, num2) => {
+  return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+}
+
+const operate = (operator, num1, num2) => {
+  if (operator === '+') {
+    return add(num1, num2);
+  } else if (operator === '-') {
+    return subtract(num1, num2);
+  } else if (operator === '*') {
+    return multiply(num1, num2);
+  } else if (operator === '/') {
+    return divide(num1, num2);
+  }
+}

ea0112f169e298f2f174a00fa610ace00e18f476
snowjelly <broadwellconnor@gmail.com>
2022-08-22 13:00:53 -0500

    massive refactor

--- script.js
+++ script.js
@@ -18,11 +18,13 @@ const logging = () => {
 }
 
 const clear = () => {
+  inputStore = [];
   userInput.numBuffer = "";
   userInput.num1 = 0;
   userInput.num2 = 0;
   userInput.operator = "";
-  userInput.prevOperator = "";
+  userInput.num1Stored = false;
+  //userInput.prevOperator = "";
   displayValue.innerHTML = "";
   displaySubValue.innerHTML = "";
   return "Cleared";
@@ -54,6 +56,8 @@ const operate = (num1, operator, num2) => {
 const equals = () => {
   if (userInput.num1 === 0 || userInput.num2 === 0) {
     return console.log("missing parameters: " + " num1= " + userInput.num1 + " operator= " + userInput.operator + " num2= " + userInput.num2);
+  } else if (typeof inputStore[1] !== 'string') {
+    
   }
   const result = operate(userInput.num1, userInput.operator, userInput.num2);
   displaySubValue.innerHTML = userInput.num1 + " " + userInput.operator + " " + userInput.num2 + " = ";
@@ -61,6 +65,10 @@ const equals = () => {
   userInput.num1 = result;
   userInput.num2 = 0; 
   //userInput.prevOperator = userInput.operator;
+  // inputStore.shift();
+  // inputStore.shift();
+  // inputStore.shift();
+  // inputStore.shift();
   return result;
 }
 
@@ -70,17 +78,18 @@ const initNum1 = () => {
   displayValue.innerHTML = "";
   displaySubValue.innerHTML = userInput.num1 + " " + userInput.operator;
   userInput.num1Stored = true;
-  userInput.prevOperator = userInput.operator;
+  //userInput.prevOperator = userInput.operator;
   return userInput.num1;
 }
 
 const initNum2 = () => {
   if (userInput.numBuffer === "") {
-    return userInput.num1;
+    return console.log('fail');
+  } else {
+    userInput.num2 = parseInt(userInput.numBuffer);
+    userInput.numBuffer = "";
+    return userInput.num2;
   }
-  userInput.num2 = parseInt(userInput.numBuffer);
-  userInput.numBuffer = "";
-  return userInput.num2;
 }
 
 const chain = () => {
@@ -94,6 +103,10 @@ const chain = () => {
   userInput.num1 = result;
   userInput.num2 = 0; 
   userInput.prevOperator = userInput.operator;
+  // inputStore.shift();
+  // inputStore.shift();
+  // inputStore.shift();
+  // inputStore.shift();
   return result;
 }
 
@@ -103,31 +116,69 @@ const log = (e) => {
     
   if (btnValue === "C") {
     clear();
-  } else if (btnValue === "⌫") {
+  }
+  else if (btnValue === "⌫") {
     backspace();
-  } else if ((btnValue === "÷" || btnValue === "×" || btnValue === "-" || btnValue === "+")) {
+  }
+  else if ((btnValue === "÷" || btnValue === "×" || btnValue === "-" || btnValue === "+")) {
+
     if (typeof inputStore[inputStore.length - 1] === 'string' && userInput.numBuffer === "") {
       logging();
       return console.log('cant do that fr');
     }
-    userInput.operator = btnValue;
-    //logging();
-
-    if (userInput.num1Stored === false) { //init num1 on first run
-      inputStore.push(userInput.num1 = initNum1());
-    } else { //chaining operators
-      inputStore.push(userInput.num2 = initNum2());
-      inputStore.push("="); 
-      inputStore.push(chain()); 
+    else {
+      userInput.operator = btnValue;
     }
-    inputStore.push(btnValue);
-    inputStore.operator = btnValue;
-    logging();
+
+
+    //init num1 on first run
+    if (userInput.num1Stored === false) { 
+      inputStore.push(userInput.num1 = initNum1());
+      inputStore.push(btnValue);
+      return console.log('num1 initialized');
+      } 
+    
+    //if the current array value is a number push the operator after
+    if (typeof inputStore[inputStore.length - 1] === 'number') {
+      if (typeof userInput.prevOperator === 'undefined') {
+        inputStore.push(userInput.operator);
+      }
+      inputStore.push(userInput.prevOperator);
+      //inputStore.push(userInput.num2 = initNum2());
+    }
+
+    //if it isnt the first run and the current array value is a string then calculate
+    if (userInput.num1Stored && typeof inputStore[inputStore.length - 1] === 'string') {
+      inputStore.push(userInput.num2 = initNum2());
+      inputStore.push("=");
+      inputStore.push(1337);
+      logging();
+    }
+    
+
+
+
+
+
+
+
+    userInput.prevOperator = userInput.operator;
   } else if (btnValue === "=") {
-    inputStore.push(userInput.num2 = initNum2());
-    inputStore.push("=");
-    inputStore.push(equals()); //equals()
-    logging();
+
+    if ((inputStore[inputStore.length - 1] === userInput.operator || inputStore[inputStore.length - 2] === "=") && userInput.numBuffer !== "") {
+
+      if (inputStore[inputStore.length - 2] === "=") {
+        inputStore.push(userInput.operator);
+    }
+      inputStore.push(userInput.num2 = initNum2());
+      inputStore.push(btnValue);
+      inputStore.push(1337);
+    }
+    else {
+      return console.log('cant do that');
+    }
+    // inputStore.push(equals()); //equals()
+     logging();
   } else if (isNaN(parseInt(btnValue)) !== true){ //if a number is clicked
     userInput.numBuffer = userInput.numBuffer.concat(btnValue);
     console.log(userInput.numBuffer);
